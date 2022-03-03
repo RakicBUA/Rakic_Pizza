0373c7490e8fe533c514434b636011ba705c0795
Rakic_Jugo <rakic.nemanja@hak-liezen.at>
2022-03-04 00:41:30 +0100

    New Views,Controller .....

---
+++ Rakic_Pizza/Controllers/FoodController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rakic_Pizza.Models;
+
+namespace Rakic_Pizza.Controllers
+{
+    public class FoodController : Controller
+    {
+        // GET: Essen
+        public ActionResult Index()
+        {
+            var food = new Food() { Name = "Hawai Pizza" };
+
+            return View();
+        }
+    }
+}
